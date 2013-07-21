dd77e207d3375447c50b504196e8cfd88b45ac15
Aron Carroll <self@aroncarroll.com>
2013-07-21 22:58:31 +0200

    Add a basic view class

---
+++ public/js/view.js
@@ -0,0 +1,27 @@
+describe(['events', 'vendor/soak', 'jquery'], function () {
+  var jQuery = require('jquery'),
+      soak = require('vendor/soak'),
+      Events = require('events');
+
+  return soak.inherits(Events, {
+    constructor: function View(element, options) {
+      Object.apply(this, arguments);
+      this.el = element || document.createElement('div');
+      this.$el = jQuery(element);
+      this.options = soak.mixin({}, this.constructor.options, options);
+      this.addEvents(this.constructor.events || {});
+    },
+    render: function () {
+      return this.el;
+    },
+    remove: function () {
+      this.$remove();
+    },
+    addEvents: function (events) {
+      Object.keys(events).forEach(function (key) {
+        var parts = key.split(' '), method = this[events[key]];
+        this.$el.on(parts.shift(), parts.join(' '), jQuery.proxy(method, this));
+      }, this);
+    }
+  });
+});
